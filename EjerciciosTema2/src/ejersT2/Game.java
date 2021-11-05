396b1a67634c8ff6af779082e8cb6b0822ac8567
jjguillen <jjavierguillen@gmail.com>
2021-11-05 18:54:48 +0100

    Metodos

---
+++ EjerciciosTema2/src/ejersT2/Game.java
@@ -0,0 +1,24 @@
+package ejersT2;
+
+public class Game {
+
+	public static void main(String[] args) {
+		
+		int vidaJugador=0, vidaEnemigo=0;
+		int poderJugador=0, poderEnemigo=0;
+		
+		//Pedir por teclado la vida del jugador y su poder
+		//Pedir por teclado la vida del enemigo y su poder
+		
+		//Hacer un menú que se repita con tres opciones 
+		//1.Golpea jugador
+		//2.Golpea enemigo
+		//3.Salir
+		//Se debe repetir hasta salir, o cuando enemigo o jugador tenga vida a cero
+		//Se debe ir mostrando la vida del jugador y del enemigo
+		
+		
+
+	}
+
+}
