a04a49110d789d0fd74da586f7ff171dd7b2a2e5
jjguillen <jjavierguillen@gmail.com>
2021-11-19 18:56:28 +0100

    shuffle

---
+++ EjerciciosTema3/src/ejersT3/Shuffle.java
@@ -0,0 +1,13 @@
+package ejersT3;
+
+public class Shuffle {
+
+	public static void main(String[] args) {
+		//Desordena un array
+		
+		//Nos crearemos una función shuffle, que para cada posición del array,
+		//intercambie el valor en una posición aleatoria por la posición siguiente.
+
+	}
+
+}
