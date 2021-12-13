809c0dcf3204d2a45ec360fdc3db9cd7f8d5840d
= <=>
2021-12-13 23:03:19 +0100

    repaso

---
+++ EjerciciosTema3/src/EjerciciosClase/Ejercicio5Examen.java
@@ -0,0 +1,33 @@
+package EjerciciosClase;
+
+public class Ejercicio5Examen {
+
+	public static int busqueda(int[]vector, int n) {
+		int i=0;
+		int j=n-1;
+		int mid=0;
+		while(i<j){
+			mid=i+(i-j)/2;
+
+			if (vector[mid]==j) {
+				break;
+			}
+			if (vector[mid]<j) {
+				i=mid+1;
+			}else if(vector[mid]>j) {
+				j=mid-1;
+			}else {
+				return -1;
+			}
+
+		}
+		return mid;
+
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
