7a68459458fee31a93b6c1c14be4c9cbf643526f
jjguillen <jjavierguillen@gmail.com>
2022-02-02 21:03:26 +0100

    Tema5

---
+++ EjerciciosTema5/src/ejersClase/TestAcceso.java
@@ -0,0 +1,13 @@
+package ejersClase;
+
+public class TestAcceso {
+
+	public static void main(String[] args) {
+		Acceso ac = new Acceso("jAvier");
+		System.out.println(ac.getNombre());
+		
+		System.out.println(ac.toString());
+
+	}
+
+}
