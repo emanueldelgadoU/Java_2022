1d8121b086e18e0e0e49273c4e19207c503786ea
jjguillen <jjavierguillen@gmail.com>
2022-02-07 21:57:12 +0100

    Ejemplo clase ArrayList Expediente

--- EjerciciosTema5/src/ejersClase/TestExpediente.java
+++ EjerciciosTema5/src/ejersClase/TestExpediente.java
@@ -1,13 +1,19 @@
 package ejersClase;
 
+import java.time.LocalDate;
+
 public class TestExpediente {
 
 	public static void main(String[] args) {
-		Expediente alumno1 = new Expediente();
-		System.out.println(alumno1.getId());
+		Expediente alumno1 = new Expediente("Carlos","Guerrero", LocalDate.of(2000, 2,20), 1);
+		alumno1.setSexo('h');
+		System.out.println(alumno1);
 		
-		Expediente alumno2 = new Expediente();
-		System.out.println(alumno2.getId());
+		alumno1.getNotas().add(new NotasCurso("Programacion",1,5,6,7));
+		alumno1.getNotas().add(new NotasCurso("Bases de datos",1,5,4,3));
+		System.out.println(alumno1);
+		
+		
 
 	}
 
