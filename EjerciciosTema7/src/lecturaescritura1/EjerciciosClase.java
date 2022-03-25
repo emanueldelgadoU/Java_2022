072857056c0a0a848950bf71f9439c02a1a76152
jjguillen <jjavierguillen@gmail.com>
2022-03-25 16:26:45 +0100

    otro

--- EjerciciosTema7/src/lecturaescritura1/EjerciciosClase.java
+++ EjerciciosTema7/src/lecturaescritura1/EjerciciosClase.java
@@ -3,7 +3,7 @@ package lecturaescritura1;
 public class EjerciciosClase {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo2");
 
 	}
 
