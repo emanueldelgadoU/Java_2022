33909e032833c51b52f593b8cf1746c1c2b68658
jjguillen <jjavierguillen@gmail.com>
2022-03-25 17:00:19 +0100

    codigobueno

--- EjerciciosTema7/src/lecturaescritura1/EjerciciosClase.java
+++ EjerciciosTema7/src/lecturaescritura1/EjerciciosClase.java
@@ -3,7 +3,7 @@ package lecturaescritura1;
 public class EjerciciosClase {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo2");
+		System.out.println("Este código es el bueno");
 
 	}
 
